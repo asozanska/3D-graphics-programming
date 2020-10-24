5e428539ce59e557c2d695c488f42eb81c071c7a
adas <ada.sozanska@gmail.com>
2020-10-24 12:53:56 +0200

    Add Uniforms exercise

---
+++ src/Exercises/Uniforms/app.cpp
@@ -0,0 +1,98 @@
+//
+// Created by pbialas on 25.09.2020.
+//
+
+#include "app.h"
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+
+#include "Application/utils.h"
+
+void SimpleShapeApplication::init() {
+
+    std::vector<GLushort> indices = {
+            0, 1, 2, 3, 4, 5, 5, 6, 3
+    };
+    GLuint idx_buffer_handle;
+    glGenBuffers(1,&idx_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(),
+                 GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    auto program = xe::create_program(std::string(PROJECT_DIR) + "/shaders/base_vs.glsl",
+                                      std::string(PROJECT_DIR) + "/shaders/base_fs.glsl");
+
+    if (!program) {
+        std::cerr << "Cannot create program from " << std::string(PROJECT_DIR) + "/shaders/base_vs.glsl" << " and ";
+        std::cerr << std::string(PROJECT_DIR) + "/shaders/base_fs.glsl" << " shader files" << std::endl;
+    }
+
+    GLuint ubo_handle(0u);
+    glGenBuffers(1,&ubo_handle);
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo_handle);
+
+    float light_intensity = 0.9;
+    float light_color[3] = {0.3, 0.1, 0.4};
+
+    glBufferData(GL_UNIFORM_BUFFER, 8 * sizeof(float), nullptr, GL_STATIC_DRAW);
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(float), &light_intensity);
+    glBufferSubData(GL_UNIFORM_BUFFER, 4 * sizeof(float), 3 * sizeof(float), light_color);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    glBindBufferBase(GL_UNIFORM_BUFFER, 0, ubo_handle);
+
+    std::vector<GLfloat> vertices = {
+            0.6f, -0.5f, 0.0f, 0.2, 0.3, 0.7,
+            0.2f, 0.1f, 0.0f, 0.2, 0.3, 0.7,
+            -0.2f, -0.5f, 0.0f, 0.2, 0.3, 0.7,
+
+            0.6f, -0.5f, 0.0f,  0.9, 0.7, 0.2,
+            -0.2f, -0.5f, 0.0f,  0.9, 0.7, 0.2,
+
+            -0.2f, -0.9f, 0.0f, 0.9, 0.7, 0.2,
+            0.6f, -0.9f, 0.0f, 0.9, 0.7, 0.2,
+
+    };
+
+    GLuint v_buffer_handle;
+    glGenBuffers(1, &v_buffer_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glGenVertexArrays(1, &vao_);
+    glBindVertexArray(vao_);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    auto u_modifiers_index = glGetUniformBlockIndex(program, "Modifiers");
+    if (u_modifiers_index == GL_INVALID_INDEX) {
+        std::cout << "Cannot find Modifiers uniform block in program" << std::endl;
+    }
+    else {
+        glUniformBlockBinding(program, u_modifiers_index, 0);
+    }
+
+    glClearColor(0.81f, 0.81f, 0.8f, 1.0f);
+    int w, h;
+    std::tie(w, h) = frame_buffer_size();
+    glViewport(0, 0, w, h);
+
+    glEnable(GL_DEPTH_TEST);
+    glUseProgram(program);
+}
+
+void SimpleShapeApplication::frame() {
+    glBindVertexArray(vao_);
+    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_SHORT, reinterpret_cast<GLvoid *>(0));
+    glBindVertexArray(0);
+}
